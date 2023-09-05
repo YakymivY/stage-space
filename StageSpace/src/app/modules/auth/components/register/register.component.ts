2a5edab9292d644692ea335a50efc6a48894fb71
Yuriy Yakymiv <yuriyyakymiv@Yuriys-MacBook-Air.local>
2023-09-05 17:01:31 +0300

    registration form improvement

--- StageSpace/src/app/modules/auth/components/register/register.component.ts
+++ StageSpace/src/app/modules/auth/components/register/register.component.ts
@@ -12,42 +12,98 @@ export class RegisterComponent {
   //role: string = 'actor';
   //roleActive: boolean = true; //true = actor, false = director
 
-  name: string = '';
-  surname: string = '';
-  birthdate: any;
-  email: string = '';
-  password: string = '';
-  confirm: string = '';
+  // name: string = '';
+  // surname: string = '';
+  // birthdate: any;
+  // email: string = '';
+  // password: string = '';
+  // confirm: string = '';
 
-  error: string = '';
+  // error: string = '';
 
   registerForm1 = new FormGroup({
-    name: new FormControl(''),
-    surname: new FormControl(''),
-    birthdate: new FormControl(''),
-    institution: new FormControl(''),
-    status: new FormControl(''),
-    email: new FormControl(''),
-    password: new FormControl(''),
-    confirm: new FormControl(''),
-    phone: new FormControl('')
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    surname: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    birthdate: new FormControl('', Validators.required),
+    institution: new FormControl('', Validators.required), /////////////////////
+    status: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    confirm: new FormControl('', [Validators.required, this.passwordMatchValidator.bind(this)]),
+    phone: new FormControl('', Validators.required) ////////////////
   });
 
   registerForm2 = new FormGroup({
-    proffesion: new FormControl('')
-  });
-
-  registerForm3 = new FormGroup({
+    proffesion: new FormControl('', Validators.required),
     works: new FormControl(''),
     noExperience: new FormControl('')
   });
 
-  registerForm4 = new FormGroup({
-    phoneCode: new FormControl(''),
-    emailCode: new FormControl('')
+  registerForm3 = new FormGroup({
+    phoneCode: new FormControl('', Validators.required),
+    emailCode: new FormControl('', Validators.required)
   });
 
+  currentStep: number = 1;
+  haveExperience: boolean = true;
+  valuesArray: Object[] = [];
+
+
   constructor (private service: AuthService) {}
+
+
+  get name () {
+    return this.registerForm1.get('name');
+  }
+  get surname () {
+    return this.registerForm1.get('surname');
+  }
+  get birthdate () {
+    return this.registerForm1.get('birthdate');
+  }
+  get institution () {
+    return this.registerForm1.get('institution');
+  }
+  get status () {
+    return this.registerForm1.get('status');
+  }
+  get email () {
+    return this.registerForm1.get('email');
+  }
+  get password () {
+    return this.registerForm1.get('password');
+  }
+  get confirm () {
+    return this.registerForm1.get('confirm');
+  }
+  get phone () {
+    return this.registerForm1.get('phone');
+  }
+  //
+  get proffesion () {
+    return this.registerForm1.get('proffesion');
+  }
+  get works () {
+    return this.registerForm1.get('works');
+  }
+  get no_experience () {
+    return this.registerForm1.get('no-experience');
+  }
+  //
+  get phoneCode () {
+    return this.registerForm1.get('phoneCode');
+  }
+  get emailCode () {
+    return this.registerForm1.get('emailCode');
+  }
+  //
+
+  passwordMatchValidator(control: FormControl): { [key: string]: boolean } | null {
+    const password = this?.registerForm1?.get('password')?.value;
+    const confirmPassword = control.value;
+
+    return password === confirmPassword ? null : { 'passwordMismatch': true };
+  }
 
   // actorPill() {
   //   this.roleActive = true;
@@ -60,6 +116,9 @@ export class RegisterComponent {
   // }
 
   onSubmit() {
+    if (this.registerForm3.valid) {
+      console.log('we can submit form')
+    }
     // if (this.password !== this.confirm) {
     //   this.error = "Passwords are not the same";
     // } else {
@@ -80,6 +139,18 @@ export class RegisterComponent {
     //     );
     //   }
     // }
+  }
+
+  nextStep(form: FormGroup) {
+    if (this.currentStep < 3) {
+      if (form.valid) this.currentStep++;
+    }
+    console.log(form.value);
+    this.valuesArray.push(form.value)
+  }
+
+  onExperience() {
+    this.haveExperience = !this.haveExperience;
   }
 
 }
